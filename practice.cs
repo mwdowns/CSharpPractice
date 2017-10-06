3a682a773e6822dce78fe4345849b24b88c65c27
Matthew Downs <mwdowns@gmail.com>
2017-10-06 12:56:01 -0400

    hello world code

--- practice.cs
+++ practice.cs
@@ -1,1 +1,15 @@
-hello
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+    }
+}
